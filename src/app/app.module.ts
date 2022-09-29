ca0b8f811ae659058ed25307c2fb680423ca37d9
Heitor Soares <heytorpg7@gmail.com>
2022-09-29 01:20:23 -0300

    component-personlizado added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
@@ -10,6 +9,8 @@ import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.co
 import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 import { FormsModule } from '@angular/forms';
 import { RenderingListsComponent } from './rendering-lists/rendering-lists.component';
+import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { RenderingListsComponent } from './rendering-lists/rendering-lists.compo
     MultiplicaPorPipe,
     PipeCustomizadoComponent,
     TwoWayBindingComponent,
-    RenderingListsComponent
+    RenderingListsComponent,
+    ComponentePersonalizadoComponent,
   ],
   imports: [
     BrowserModule,
